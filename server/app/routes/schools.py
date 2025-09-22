9ca84d2a04a9990a10cd9d80179a0f6ab1690a8d
Chei <cochewaki@gmail.com>
2025-09-22 08:37:51 +0300

    added auth, users, school endpoints

--- server/app/routes/schools.py
+++ server/app/routes/schools.py
@@ -1,3 +1,12 @@
 from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+from marshmallow import ValidationError
+
+from models.school import School
+from models.user import User, ROLES
+from models.base import db
+from schemas.schools import SchoolSchema
+from schemas.user import UserSchema
+from utils.responses import success_response, error_response
+
